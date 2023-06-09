ec97670ff88aca43a6a1354e84480128298d5d09
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-06-09 19:22:46 +0000

    4 tasks done

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    length = len(my_list)
+    a = my_list.copy()
+    if idx < 0:
+        return a
+    elif idx > length:
+        return a
+    else:
+        a[idx] = element
+        return a
