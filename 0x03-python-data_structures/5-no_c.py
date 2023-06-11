f6d3e77447d42b28cb4c0e06a9c5743c1a31c903
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-06-11 17:00:03 +0000

    5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/pyton3
+def no_c(my_string):
+    s = ""
+    for element in my_string:
+        if element == 'c' or element == 'C':
+            continue
+        s += element
+    return s
