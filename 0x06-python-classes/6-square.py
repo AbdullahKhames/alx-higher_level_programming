0890a8f5dd666dafeb4db8efefd74fb8e0f8bd67
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-06-30 17:57:07 +0300

    last task mandatiory

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -72,6 +72,9 @@ def validate_size(value):
 
 
 def validate_position(value):
-    if not isinstance(value, int) or value < 0:
+    if not isinstance(value, tuple) or len(value) != 2:
+        raise TypeError("position must be a tuple of 2 positive integers")
+    if not all(isinstance(num, int) and num >= 0 for num in value):
         raise TypeError("position must be a tuple of 2 positive integers")
     return True
+
