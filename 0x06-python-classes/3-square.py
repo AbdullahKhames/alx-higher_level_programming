55419c403a72b5098423ba7a6023b41b57a4579d
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-06-30 16:28:13 +0300

    new task

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -10,3 +10,18 @@ class Square:
     new class
     """
 
+    def __init__(self, size=0):
+        """
+        init method
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        mthod to calculate area of square
+        """
+        return self.__size * self.__size
