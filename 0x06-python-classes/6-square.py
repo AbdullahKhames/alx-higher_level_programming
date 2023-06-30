c096e11d48ff6aba94de928f4c38918431290c0e
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-06-30 17:44:23 +0300

    last task mandatiory

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -9,15 +9,14 @@ class Square:
     class represents square characteristics
     """
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """
         init method
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        if validate_size(size):
+            self.__size = size
+        if validate_position(position[0]) and validate_position(position[1]):
+            self.__position = position
 
     def area(self):
         """
@@ -37,6 +36,15 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value=(0,0)):
+        if validate_position(value[0]) and validate_position(value[1]):
+            self.__position = value
+
     def my_print(self):
         """
         function to print the squares with the # notation
@@ -46,6 +54,22 @@ class Square:
             return
 
         for i in range(self.size):
+            for c in range(self.position[0]):
+                print(" ", end="")
             for j in range(self.size):
                 print("#", end="")
             print()
+
+
+def validate_size(value):
+    if not isinstance(value, int):
+        raise TypeError("size must be an integer")
+    if value < 0:
+        raise ValueError("size must be >= 0")
+    return True
+
+
+def validate_position(value):
+    if not isinstance(value, int) and value < 0:
+        raise TypeError("position must be a tuple of 2 positive integers")
+    return True
