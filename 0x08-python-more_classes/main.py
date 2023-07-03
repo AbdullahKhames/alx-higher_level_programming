d0ab762b91f0a708f604fea91468dd9702de0ed0
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-07-03 23:20:04 +0300

    3 - rectangle 3rd  task

--- 0x08-python-more_classes/main.py
+++ 0x08-python-more_classes/main.py
@@ -10,3 +10,18 @@ print(r.width)
 print(r.height)
 print(r.__dict__)
 print(r.area())
+
+Rectangle1 = __import__('3-rectangle').Rectangle
+
+my_rectangle = Rectangle1(2, 4)
+print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+print(str(my_rectangle))
+print(repr(my_rectangle))
+
+print("--"*50)
+
+my_rectangle.width = 10
+my_rectangle.height = 3
+print(my_rectangle)
+print(repr(my_rectangle))
