8a2c98cd1a7dc3e79869f3ff5cdac95334128619
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-07-03 23:31:26 +0300

    5 - rectangle 5th  task

--- 0x08-python-more_classes/main.py
+++ 0x08-python-more_classes/main.py
@@ -51,3 +51,15 @@ print("--")
 
 print(new_rectangle is my_rectangle)
 print(type(new_rectangle) is type(my_rectangle))
+
+Rectangle = __import__('5-rectangle').Rectangle
+
+my_rectangle = Rectangle(2, 4)
+print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+del my_rectangle
+
+try:
+    print(my_rectangle)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
