706814f6f15709902ad0e38af67fa3c5cc94768f
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-07-03 23:38:50 +0300

    6 - rectangle 6th  task

--- 0x08-python-more_classes/main.py
+++ 0x08-python-more_classes/main.py
@@ -63,3 +63,13 @@ try:
     print(my_rectangle)
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
+
+    Rectangle = __import__('6-rectangle').Rectangle
+
+    my_rectangle_1 = Rectangle(2, 4)
+    my_rectangle_2 = Rectangle(2, 4)
+    print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
+    del my_rectangle_1
+    print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
+    del my_rectangle_2
+    print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
