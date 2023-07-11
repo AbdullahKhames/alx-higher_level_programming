e3fd267c6c27ce3cfd6ad6176824db74e3a9f987
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-07-11 19:10:45 +0300

    student module

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+student mod
+"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def __str__(self):
+        return "[Student] firstname: {}, lastname: {}, age: {}"\
+            .format(self.first_name, self.last_name, self.age)
+
+    def to_json(self, attrs=None):
+        dict_obj = {}
+        if attrs is None:
+            return self.__dict__
+        else:
+            for att in attrs:
+                dict_obj = {att: getattr(self, att)}
+            return dict_obj
