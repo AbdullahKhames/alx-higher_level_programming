e1b5664eb218160f0b5f4c4ff1ec0efb5e42c75a
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-07-11 18:28:17 +0300

    add item to a list

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+ 7. Load, add, save """
+import json
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+filename = "add_item.json"
+ls = []
+for count, item in enumerate(sys.argv):
+    if count == 0:
+        continue
+    ls.append(item)
+other_ls = []
+try:
+    other_ls = load_from_json_file(filename)
+except json.JSONDecodeError as e:
+    print(e)
+if type(other_ls) is list and len(other_ls) != 0:
+    ls.extend(other_ls)
+save_to_json_file(ls, filename)
+print(ls)
