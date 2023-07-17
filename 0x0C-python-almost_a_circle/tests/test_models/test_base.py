9e248dfaff4fe7f395df872378f6a50014d8c51a
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-07-17 21:09:52 +0300

    some refactoring

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python3
+"""Defines unittests for base.py.
+Unittest classes:
+    TestBase_instantiation - line 23
+    TestBase_to_json_string - line 110
+    TestBase_save_to_file - line 156
+    TestBase_from_json_string - line 234
+    TestBase_create - line 288
+    TestBase_load_from_file - line 340
+    TestBase_save_to_file_csv - line 406
+    TestBase_load_from_file_csv - line 484
 """
-test area method
-"""
+import os
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
 class BaseTestCase(unittest.TestCase):
+    """Unittests for testing instantiation of the Base class."""
     def test_id_generator(self):
         base1 = Base()
         base2 = Base()
