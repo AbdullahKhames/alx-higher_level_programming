671527081ececc4c936c1a3d48a9b21162025003
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-08-16 15:06:58 +0300

    new

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,9 +1,9 @@
 -- script to listt geres id by show
-SELECT s.title, g.id
+SELECT s.title, sg.id
 FROM tv_shows s
 INNER JOIN tv_show_genres sg
 ON s.id = sg.show_id
 INNER JOIN tv_genres g
 ON sg.genre_id = g.id
-ORDER BY s.title, g.id
+ORDER BY s.title, sg.id
 ASC;
