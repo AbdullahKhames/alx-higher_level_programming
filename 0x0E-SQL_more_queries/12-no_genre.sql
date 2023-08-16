9730f9abfe2058adbf5bb05e8dfa644164d7344d
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-08-16 15:28:16 +0300

    where field is null

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- outer joinn where field is null
+SELECT s.title, sg.genre_id
+FROM tv_shows s
+LEFT OUTER JOIN tv_show_genres sg
+ON s.id = sg.genre_id
+WHERE sg.genre_id IS NULL
+ORDER BY s.title, sg.genre_id
+ASC;
