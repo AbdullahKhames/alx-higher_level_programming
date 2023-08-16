854e9fe46407044d8d4544792950b60bb3959334
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-08-16 16:02:44 +0300

    genres and shows alll together

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,10 @@
+-- script to list all shows in genres
+SELECT s.title
+FROM tv_shows s
+INNER JOIN tv_show_genres sg
+ON s.id = sg.show_id
+INNER JOIN tv_genres g
+ON sg.genre_id = g.id
+WHERE g.name = 'Comedy'
+ORDER BY s.title
+ASC;
---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- script to list all shows, and all genres linked to that show
+SELECT s.title, g.name
+FROM tv_shows s
+INNER JOIN tv_show_genres sg
+ON s.id = sg.show_id
+INNER JOIN tv_genres g
+ON sg.genre_id = g.id
+ORDER BY s.title, g.name
+ASC;
