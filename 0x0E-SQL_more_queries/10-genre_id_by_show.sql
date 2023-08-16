727df50d4eca5ccd5ae55ed6f2125e6d611de06d
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-08-16 15:18:41 +0300

    fixing bugs

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,9 +1,9 @@
 -- script to listt geres id by show
-SELECT s.title, sg.id AS genre_id
+SELECT s.title, sg.genre_id AS genre_id
 FROM tv_shows s
 INNER JOIN tv_show_genres sg
 ON s.id = sg.show_id
 INNER JOIN tv_genres g
 ON sg.genre_id = g.id
-ORDER BY s.title, sg.id
+ORDER BY s.title, sg.genre_id
 ASC;
