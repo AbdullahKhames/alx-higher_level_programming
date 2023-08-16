8a88524e5316e91ec86affb82f33b949d1bc00c4
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-08-16 15:32:40 +0300

    fixed

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -2,7 +2,7 @@
 SELECT s.title, sg.genre_id
 FROM tv_shows s
 LEFT OUTER JOIN tv_show_genres sg
-ON s.id = sg.genre_id
+ON s.id = sg.show_id
 WHERE sg.genre_id IS NULL
 ORDER BY s.title, sg.genre_id
 ASC;
