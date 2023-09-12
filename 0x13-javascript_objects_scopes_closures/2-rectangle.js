0bb00e91f50d5f608cf73abf3c84b690cc489090
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-09-12 20:13:55 +0300

    empty object

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-  width;
-  height;
   constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      return;
+    }
     this.width = w;
     this.height = h;
   }
