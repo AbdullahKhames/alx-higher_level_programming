dc41af7827a392015ebb1f693d46f52d34528bda
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-09-12 23:47:45 +0300

    new

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const dict = require('./101-data.js').dict;
+const newDic = {};
+for (const prop in dict) {
+  if (!newDic[dict[prop]]) {
+    newDic[dict[prop]] = [];
+  }
+  newDic[dict[prop]].push(prop);
+}
+console.log(newDic);
