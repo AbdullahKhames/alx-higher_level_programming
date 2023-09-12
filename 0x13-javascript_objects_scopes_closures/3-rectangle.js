a9a516ff6cfa1f4f60a4c06874364a88a7b201d7
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-09-12 20:21:37 +0300

    print

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || w === undefined || h <= 0 || h === undefined) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'x';
+      }
+      console.log(row);
+    }
+  }
+};
