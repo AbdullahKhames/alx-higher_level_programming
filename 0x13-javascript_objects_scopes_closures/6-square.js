16ddd880a37922523db3f0806d18aebf663bd216
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-09-12 22:34:56 +0300

    reverse

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+const otherSquare = require('./5-square');
+
+module.exports = class Square extends otherSquare {
+  charPrint (ch) {
+    if (ch === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        let row = '';
+        for (let j = 0; j < this.width; j++) {
+          row += ch;
+        }
+        console.log(row);
+      }
+    }
+  }
+};
--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+exports.esrever = function (list) {
+  const arr = [];
+  let j = 0;
+  for (let i = list.length - 1; i >= 0; i--, j++) {
+    arr[j] = list[i];
+  }
+  return arr;
+};
