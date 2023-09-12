03ea6e07f8cf6e5f4406655e8e7da2c7188c5c98
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-09-12 23:11:36 +0300

    new

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-const ls = require('100-data.js').list;
-
-const mapped = this.ls.map((x, index) => {
+const ls = require('./100-data.js').list;
+const mapped = ls.map((x, index) => {
   return x * index;
 });
 console.log(ls);
