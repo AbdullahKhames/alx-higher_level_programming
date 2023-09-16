4a750c9ccbd378a96c2b9f22b89ce70caa0e06e6
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-09-16 12:29:15 +0300

    new

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+to demonstrate mysql db connection using DBAPI called MySQLdb
+"""
+from sys import argv
+import MySQLdb
+
+
+def filterStates(uName, pwd, dbName):
+    """
+    filterStates function
+    """
+    dv = MySQLdb.connect(host='localhost', user=uName, password=pwd,
+                         database=dbName, port=3306)
+    sql_query = """
+SELECT * FROM states s WHERE
+ s.name LIKE 'N%' ORDER BY s.id ASC
+    """
+    cur = dv.cursor()
+    cur.execute(sql_query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+
+if __name__ == '__main__':
+    """
+    prevents script from running when imported
+    """
+    filterStates(argv[1], argv[2], argv[3])
