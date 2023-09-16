0b6633560adfb75620d6c4b1c48fa886d3e78f3e
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-09-16 12:46:44 +0300

    search

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+to demonstrate mysql db connection using DBAPI called MySQLdb
+"""
+from sys import argv
+import MySQLdb
+
+
+def searchStates(uName, pwd, dbName, searched):
+    """
+    searchStates function
+    """
+    dv = MySQLdb.connect(host='localhost', user=uName, password=pwd,
+                         database=dbName, port=3306)
+    sql_query = """
+SELECT * FROM states s WHERE
+ s.name = %s ORDER BY s.id ASC
+    """
+    cur = dv.cursor()
+    cur.execute(sql_query, (searched, ))
+    row = cur.fetchone()
+    print(row)
+
+
+if __name__ == '__main__':
+    """
+    prevents script from running when imported
+    """
+    searchStates(argv[1], argv[2], argv[3], argv[4])
