ab43735beea4eb023c282cf2d05ff07308d2cacf
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-09-17 00:46:00 +0300

    new

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+module to describe state model
+"""
+from sqlalchemy import Column, INTEGER, String, create_engine
+from sqlalchemy.orm import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    describing the domain model
+    """
+    __tablename__ = 'states'
+    id = Column(INTEGER, primary_key=True)
+    name = Column(String(128), nullable=False)
+
+if __name__ == '__main__':
+    engine = create_engine('mysql://root:1395760@localhost:3306/hbtn_0e_6_usa')
+    Base.metadata.create_all(engine)
--- 0x0F-python-object_relational_mapping/test.py
+++ 0x0F-python-object_relational_mapping/test.py
@@ -1,18 +1,44 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, MetaData,text
+from sqlalchemy import Table, Column
+from sqlalchemy import Integer, String
+from sqlalchemy import DateTime, ForeignKey, inspect, and_, or_
+from sqlalchemy.orm import declarative_base, Session
+Base = declarative_base()
+
+class Network(Base):
+    __tablename__ = 'networks2'
+    id = Column(Integer, primary_key=True)
+    name =  Column(String(100), nullable=False)
 
 
 def run_query():
     engine = create_engine("mysql://root:1395760@localhost/test")
-    
+    con = engine.connect()
+    metadata = MetaData()
+    metadata.reflect(bind=engine)
+    table = metadata.tables['app_users']
+    select_statement = table.select().where(or_(table.c.name == 'edward', and_(table.c.fullname == 'ed', table.c.id > 5)))
+    print(select_statement)
+    result = con.execute(select_statement)
+    rows = result.fetchall()
+    for row in rows:
+        print(row)
+    con.commit()
 
-    with engine.connect() as conn:
-        result = conn.execute(text("INSERT INTO employee values(1, 'dilbert')"))
-        result = conn.execute(text("SELECT * FROM employee"))
-        rows = result.fetchall()
-        for row in rows:
-            print(row)
+def orm():
+    engine = create_engine("mysql://root:1395760@localhost/test")
+    Base.metadata.create_all(engine)
+    network1 = Network(name='first')
+    network2 = Network(name='second')
+    network3 = Network(name='third')
+    network4 = Network(name='fourth')
+    session = Session(bind=engine)
+    session.add_all([network1, network2, network3, network4])
+    for name in session.query(Network):
+        print(name.name)
+
 
 if __name__ == '__main__':
-    run_query()
+    orm()
