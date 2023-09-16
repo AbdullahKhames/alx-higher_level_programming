f1ce1254191a6e82734abea431c19c6bf1397210
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-09-16 15:03:22 +0300

    filter_cities

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+"""
+to demonstrate mysql db connection using DBAPI called MySQLdb
+"""
+from sys import argv
+import MySQLdb
+
+
+def filteredCities(uName, pwd, dbName, stateName):
+    """
+    filteredCities function
+    """
+    dv = MySQLdb.connect(host='localhost',
+                         user=uName,
+                         passwd=pwd,
+                         database=dbName,
+                         port=3306)
+    sql_query = 'SELECT c.name \
+        FROM cities c \
+        INNER JOIN states s \
+        ON s.id = c.state_id \
+        WHERE s.name = "{}"\
+        ORDER BY c.id ASC'.format(stateName)
+    cur = dv.cursor()
+    cur.execute(sql_query)
+    rows = cur.fetchall()
+    first_iteration = True
+
+    for row in rows:
+        if not first_iteration:
+            print(", ", end="")
+        print(row[0], end="")
+        first_iteration = False
+    print()
+
+if __name__ == '__main__':
+    """
+    prevents script from running when imported
+    """
+    filteredCities(argv[1], argv[2], argv[3], argv[4])
