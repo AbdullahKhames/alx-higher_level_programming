125baedfd7acda79659c54d9e59c95afb1b59756
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-09-16 12:15:35 +0300

    pycode style

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+
+"""
+to demonstrate mysql db connection using DBAPI called MySQLdb
+"""
 from sys import argv
 import MySQLdb
 
 
 def connect(uName, pwd, dbName):
+    """
+    connection function
+    """
     dv = MySQLdb.connect(host='localhost', user=uName, password=pwd,
                          database=dbName, port=3306)
     sql_query = 'SELECT * FROM states s ORDER BY s.id ASC'
@@ -15,4 +22,7 @@ def connect(uName, pwd, dbName):
 
 
 if __name__ == '__main__':
+    """
+    prevents script from running when imported
+    """
     connect(argv[1], argv[2], argv[3])
