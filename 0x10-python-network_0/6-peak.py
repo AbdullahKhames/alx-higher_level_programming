1bd3356bd0dd75e174e6d45d760d142e4be702ea
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-10-01 23:10:28 +0300

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ function to find_peak in a list """
 def find_peak(list_of_integers):
+    """ function to find_peak in a list """
     if list_of_integers is None or len(list_of_integers) == 0:
         return None
     min_int = list_of_integers[0]
