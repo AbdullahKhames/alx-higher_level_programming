50de9d5ae436e2b3f414c53c85933699be1f51d6
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-10-01 23:45:21 +0300

    new

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # file to connect to given domain name and get its response content lenght
-curl -s -X POST -d 'email=test@gmail.com, subject=I will always be here for PLD' "$1"
+curl -s -X POST -d '"email"="test@gmail.com", "subject"="I will always be here for PLD"' "$1"
