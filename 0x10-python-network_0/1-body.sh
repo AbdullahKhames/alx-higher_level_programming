d1c754f4c8f566f8a3f7d4938ef9a3c24275ec60
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-10-01 22:29:44 +0300

    no redirection

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -2,7 +2,6 @@
 # file to connect to given domain name and get its response content lenght
 url="$1"
 status_code=$(curl --silent --head --output /dev/null --write-out "%{http_code}" "$url")
-echo "$status_code"
 if [ "$status_code" -eq 200 ]; then
   echo "$(curl -s "$url")"
 fi
