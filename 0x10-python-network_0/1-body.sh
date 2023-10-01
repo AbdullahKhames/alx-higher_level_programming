91acbe3792a495d3b8dd762b1c449388833e9351
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-10-01 22:26:08 +0300

    edited

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # file to connect to given domain name and get its response content lenght
 url="$1"
-status_code=$(curl --head --silent --write-out "%{http_code}" "$url")
+status_code=$(curl --silent --head --output /dev/null --write-out "%{http_code}" "$url")
 echo "$status_code"
 if [ "$status_code" -eq 200 ]; then
   echo "$(curl -sL "$url")"
