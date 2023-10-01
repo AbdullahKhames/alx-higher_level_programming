27545046f9750e1152e1266aff918e89f7c0ed2f
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-10-01 23:39:34 +0300

    binary search for peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,10 +4,13 @@
 
 def find_peak(list_of_integers):
     """ function to find_peak in a list """
-    if list_of_integers is None or len(list_of_integers) == 0:
-        return None
-    min_int = list_of_integers[0]
-    for i in list_of_integers:
-        if i > min_int:
-            min_int = i
-    return min_int
+    left, right = 0, len(list_of_integers) - 1
+
+    while left < right:
+        mid = left + (right - left) // 2
+
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            right = mid
+        else:
+            left = mid + 1
+    return list_of_integers[left]
