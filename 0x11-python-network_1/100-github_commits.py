dc848f646fa669aa4446f0e865c38c390899cb33
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-10-03 02:28:38 -0700

    new

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
 """module documented"""
-from requests.auth import HTTPBasicAuth
 from requests import get
 from sys import argv
 
 
 def connect():
     """function to connect to check status documented"""
-    heads={'accept': 'application/vnd.github+json'}
-    params = {'per_page': 15}
+    heads = {'accept': 'application/vnd.github+json'}
+    params = {'per_page': 10}
     url = f'https://api.github.com/repos/{argv[2]}/{argv[1]}/commits'
-    resp = get(url, params=params, headers=heads, auth=HTTPBasicAuth('AbdullahKhames', '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'))
-    print(resp.url)
+    resp = get(url, params=params, headers=heads)
     if resp:
         text = resp.json()
-        for json_commit in text:
-            print(json_commit)
-            # print(json_commit['commit']["author"]["name"])
-            # print(json_commit['sha'])
+        for i in range(len(text)):
+            print(f"{text[i]['sha']}: {text[i]['commit']['author']['name']}")
     else:
         print(f'None')
 
