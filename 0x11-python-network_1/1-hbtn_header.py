97e9271b686d5d97516dd96c92e647098851ff9d
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-10-03 08:14:31 -0700

    x-req-id

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""module documented"""
+from urllib import request
+from sys import argv
+
+
+def connect():
+    """function to connect to check status documented"""
+
+    with request.urlopen(argv[1]) as resp:
+        body = resp.read()
+    resp_id = resp.getheader('X-Request-Id')
+    print(resp_id)
+
+
+if __name__ == '__main__':
+    """function to connect to check status documented"""
+    connect()
