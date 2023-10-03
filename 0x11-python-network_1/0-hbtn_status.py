4ef9752855c3fda23e05f9b3d7754a24e7b3fde7
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-10-03 07:46:31 -0700

    new

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+from urllib import request
+
+
+def connect():
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
+        body = resp.read()
+    char_set = resp.headers.get_content_charset()
+    decoded_body = body.decode(char_set)
+    print(decoded_body)
+
+
+if __name__ == '__main__':
+    connect()
