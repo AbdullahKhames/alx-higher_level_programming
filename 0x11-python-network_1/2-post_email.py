02040ebcc64e1c7a8a945422ee803d71d03c73a5
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-10-03 08:20:01 -0700

    new

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""module documented"""
+from urllib import request
+from sys import argv
+
+
+def connect():
+    """function to connect to check status documented"""
+    values = {'email': argv[2]}
+    values = request.parse.urlencode(values)
+    data = values.encode('utf-8')
+    req = request.Request(argv[1], data)
+    with request.urlopen(req) as resp:
+        body = resp.read()
+    resp_id = resp.getheader('X-Request-Id')
+    print(resp_id)
+
+
+if __name__ == '__main__':
+    """function to connect to check status documented"""
+    connect()
