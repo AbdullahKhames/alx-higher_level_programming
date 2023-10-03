0dd34291f9c1d54c64208583354bfcd2dcee3ba2
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-10-03 11:22:25 -0700

    json

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -14,24 +14,18 @@ def connect():
     url = 'http://0.0.0.0:5000/search_user'
     resp = post(url, data=data)
     if resp:
-        text = resp.json()
+        try:
+            text = resp.json()
+        except Exception:
+            print('Not a valid JSON')
+            return
         if not text:
             print('No result')
-            return
-        elif not is_json(text):
-            print('Not a valid JSON')
             return
         else:
             print(f'[{text["id"]}] {text["name"]}')
     else:
         print(f'Error code: {resp.status_code}')
-
-
-def is_json(text):
-    """function to check if str is json or not"""
-    if not type(text) == dict:
-        return False
-    return True
 
 
 if __name__ == '__main__':
