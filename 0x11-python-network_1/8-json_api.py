7f23fd96c43022aa4ec9d6085aaafa6d71052ea1
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-10-03 11:06:07 -0700

    json

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -15,6 +15,7 @@ def connect():
     resp = post(url, params=data)
     if resp:
         text = resp.text
+        print(text)
         if len(text) == 3:
             print('No result')
         elif not is_json(text):
