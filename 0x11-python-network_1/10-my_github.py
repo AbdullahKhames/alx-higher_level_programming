62781e886316fb3c8576c9a5d1db254aae56c574
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-10-03 11:36:08 -0700

    github id

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""module documented"""
+from requests.auth import HTTPBasicAuth
+from requests import get
+from sys import argv
+
+
+def connect():
+    """function to connect to check status documented"""
+    url = 'https://api.github.com/user'
+    resp = get(url, auth=HTTPBasicAuth(argv[1], argv[2]))
+    if resp:
+        text = resp.json()
+        print(text["id"])
+    else:
+        print(f'Error code: {resp.status_code}')
+
+
+if __name__ == '__main__':
+    """function to connect to check status documented"""
+    connect()
