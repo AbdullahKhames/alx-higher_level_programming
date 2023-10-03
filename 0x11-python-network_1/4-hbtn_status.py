de7dad495ba5706a95944232597fb62e6b71d35b
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-10-03 10:25:37 -0700

    status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""module documented"""
+from requests import get, exceptions
+
+
+def connect():
+    """function to connect to check status documented"""
+    try:
+        resp = get('https://alx-intranet.hbtn.io/status')
+        body = resp.text
+        print('Body response:')
+        print(f'\t- type: {type(body)}')
+        print(f'\t- content: {body}')
+    except exceptions.HTTPError as ex:
+        print(f'Error code: {ex.getcode()}')
+
+
+if __name__ == '__main__':
+    """function to connect to check status documented"""
+    connect()
