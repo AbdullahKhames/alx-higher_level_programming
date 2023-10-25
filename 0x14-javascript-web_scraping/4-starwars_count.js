292b503f879b28645fb0fc4f7fb6d393a49d99a7
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-10-25 08:40:19 +0300

    Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,32 @@
 #!/usr/bin/node
+const request = require('request'); // Import the 'request' library
+
+request
+  .get(process.argv[2])
+  .on('error', function (err) {
+    console.error(err);
+  })
+  .on('response', function (response) {
+    let responseData = '';
+    let counter = 0;
+    response.on('data', function (chunk) {
+      responseData += chunk;
+    });
+
+    response.on('end', function () {
+      try {
+        const data = JSON.parse(responseData);
+        const films = data.results;
+        for (const film of films) {
+          for (const character of film.characters) {
+            if (character.indexOf('18') !== -1) {
+              counter++;
+            }
+          }
+        }
+        console.log(counter);
+      } catch (error) {
+        console.error('Error parsing JSON:', error);
+      }
+    });
+  });
