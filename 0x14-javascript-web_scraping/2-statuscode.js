e57ae9a671f074cc45a7ca6723231803e9a339dc
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-10-25 07:36:27 +0300

    fixed status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,18 @@
 #!/usr/bin/node
 // get the code of a url
 const request = require('request');
-request(process.argv[2], (error, response) => {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log('code: ', response.statusCode);
-  }
-});
+// request(process.argv[2], (error, response) => {
+//   if (error) {
+//     console.error(error);
+//   } else {
+//     console.log('code: ', response.statusCode);
+//   }
+// });
+request
+  .get(process.argv[2])
+  .on('error', function (err) {
+    console.error(err);
+  })
+  .on('response', function (response) {
+    console.log('code: ' + response.statusCode);
+  });
