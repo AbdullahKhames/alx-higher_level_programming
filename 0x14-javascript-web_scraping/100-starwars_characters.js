90efe77737545ebb60d5346333af1e824adc8cc0
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-10-25 23:44:38 +0300

    starwars

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,25 @@
 #!/usr/bin/node
+const request = require('request');
+options = {
+  method: 'GET',
+  uri: `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`
+};
+request(options, (error, response, data) => {
+  if (error) {
+    console.error(error);
+  }
+  actors = JSON.parse(data).characters;
+  for (actorUrl of actors) {
+    options = {
+      method: 'GET',
+      uri: `${actorUrl}`
+    };
+    request(options, (error, response, data) => {
+      if (error) {
+        console.error(error);
+      }
+      actor = JSON.parse(data);
+      console.log(actor.name);
+    })
+  }
+});
