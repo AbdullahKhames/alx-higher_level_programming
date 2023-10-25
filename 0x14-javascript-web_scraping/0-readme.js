71cf52972807c793573bcc97cf655e9f3de52c76
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-10-25 07:20:42 +0300

    semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
-// script to read and input a command line file
-const fs = require('fs');
-const arguments = process.argv;
-const filePath = arguments[2];
-fs.readFile(filePath, 'utf8', (err, data) =>{
-    if (err){
-        console.log(err);
-        return;
-    }
-    else {
-        console.log(data);
-    }
+// script to read file
+const fileSys = require('fs');
+const fileName = process.argv[2];
+fileSys.readFile(fileName, 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+// get the code of a url
+const request = require('request');
+request(process.argv[2], (error, response) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('code: ', response.statusCode);
+  }
+});
